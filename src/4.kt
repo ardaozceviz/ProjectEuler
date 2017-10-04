c215c6c7fcc015193a296f711bf7ca2a1bcad6bd
Arda Ozceviz <ardaozceviz@hotmail.com>
2017-10-04 17:04:04 +0300

    some addings on 4

--- src/4.kt
+++ src/4.kt
@@ -2,18 +2,19 @@
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
  */
-
 fun main(args: Array<String>) {
-    var store = arrayListOf<Int>()
+    val store = arrayListOf<Int>()
     var numberOne = 9
     var numberTwo = 9
+    val resetNumberTwo = numberTwo
 
     while (numberOne >= 1) {
         while (numberTwo >= numberOne) {
-            println(numberOne * numberTwo)
+            store.add(numberOne * numberTwo)
             numberTwo--
         }
         numberOne--
-        numberTwo = 9
+        numberTwo = resetNumberTwo
     }
+    println(store)
 }
